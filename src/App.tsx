c923ae5240be4789a5d97b58ab48cb1449c332e6
Lina Hsu <linahthsu@gmail.com>
2023-11-27 11:27:22 -0300

    Adiciona o Heather no App

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,23 @@ import RecipeInProgress from './pages/RecipeInProgress';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
+import Header from './components/Header';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      <Route path="/meals" element={ <Recipes /> } />
-      <Route path="/drinks" element={ <Recipes /> } />
       <Route path="/meals/:id-da-receita" element={ <RecipeDetails /> } />
       <Route path="/drinks/:id-da-receita" element={ <RecipeDetails /> } />
       <Route path="/meals/:id-da-receita/in-progress" element={ <RecipeInProgress /> } />
       <Route path="/drinks/:id-da-receita/in-progress" element={ <RecipeInProgress /> } />
-      <Route path="/profile" element={ <Profile /> } />
-      <Route path="/done-recipes" element={ <DoneRecipes /> } />
-      <Route path="/favorite-recipes" element={ <FavoriteRecipes /> } />
+      <Route path="/meals" element={ <Header /> }>
+        <Route index element={ <Recipes /> } />
+        <Route path="/drinks" element={ <Recipes /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/done-recipes" element={ <DoneRecipes /> } />
+        <Route path="/favorite-recipes" element={ <FavoriteRecipes /> } />
+      </Route>
     </Routes>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+import { Outlet, useParams } from 'react-router-dom';
+
 function Header() {
+  const params = useParams();
+
   return (
     <div>
-      <h1>Header</h1>
+      <h1 data-testid="page-title">Título</h1>
+      <img
+        src="src/images/profileIcon.svg"
+        alt="ícone do perfil"
+        data-testid="profile-top-btn"
+      />
+      <img
+        src="src/images/searchIcon.svg"
+        alt="ícone de pesquisa"
+        data-testid="search-top-btn"
+      />
+      <Outlet />
     </div>
   );
 }

5bba351a9369795aca677ddc764bd0cf9309786a
Fabio Lessa <contatofabiolessa@gmail.com>
2023-11-28 14:30:32 -0300

    Fix estado global

--- src/redux/reducer/mainReducer.ts
+++ src/redux/reducer/mainReducer.ts
@@ -4,8 +4,8 @@ import { fetchCategoriesDrinks } from '../../utils/apiDrinks';
 
 export const INITIAL_STATE = {
   place: '',
-  drinks: [],
   meals: [],
+  drinks: [],
   drinkCategories: await fetchCategoriesMeals(),
   mealsCategories: await fetchCategoriesDrinks(),
 };
@@ -14,12 +14,16 @@ export const mainReducer = (state = INITIAL_STATE, action: AnyAction) => {
   switch (action.type) {
     case 'CHANGE_PLACE':
       return { ...state, place: action.payload };
-    case 'CHANGE_DRINKS':
-      return { ...state, drinks: action.payload };
     case 'CHANGE_MEALS':
       return { ...state, meals: action.payload };
+    case 'CHANGE_DRINKS':
+      return { ...state, drinks: action.payload };
     case 'DINAMIC_RECIPE':
-      return { ...state, [action.payload.place]: action.payload.recipe };
+      return {
+        ...state,
+        meals: [],
+        drinks: [],
+        [action.payload.place]: action.payload.recipe };
     default:
       return state;
   }

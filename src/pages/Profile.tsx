364cfc189c4b9a4d785d2733de1e816964497fd7
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-11-28 20:20:07 -0300

    Implementa função dos botões + testes (Req. 57-61)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,47 @@
+import { useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 function Profile() {
-  const userEmail = JSON.stringify(useLocalStorage('user')[0].email)
+  const navigate = useNavigate();
+
+  const user = useLocalStorage('user')[0];
+
+  const logout = () => {
+    localStorage.clear();
+    navigate('/');
+  };
 
   return (
     <div>
-      <h3 data-testid="profile-email">{ userEmail.replace(/"/g, '') }</h3>
+      <h3 data-testid="profile-email">
+        {
+        // Tive que fazer essa condicional para poder acessar '/profile' sem precisar ter feito o Login nos testes --felipe
+        user !== undefined
+          ? JSON.stringify(user.email).replace(/"/g, '')
+          : ''
+        }
+      </h3>
 
       <div>
-        <button data-testid="profile-done-btn">Done recipes</button>
-        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
-        <button data-testid="profile-logout-btn">Logout</button>
+        <button
+          data-testid="profile-done-btn"
+          onClick={ () => navigate('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          onClick={ () => navigate('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          onClick={ () => logout() }
+        >
+          Logout
+        </button>
       </div>
 
       <Footer />
---
+++ src/tests/Req-56-ao-61.test.tsx
@@ -0,0 +1,73 @@
+import { userEvent } from '@testing-library/user-event';
+import { screen } from '@testing-library/dom';
+import App from '../App';
+import renderWithRouterAndRedux from '../utils/renderWithRouterAndRedux';
+
+const user = userEvent.setup();
+
+describe('Teste a página Profile - Req. 51 ao 61', () => {
+  it('Testa se a página de perfil mostra na tela o email usado no login e 3 botões (Done Recipes, Favorite Recipes e Logout)', async () => {
+    renderWithRouterAndRedux(<App />);
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitBtn = screen.getByTestId('login-submit-btn');
+
+    const USER_EMAIL = 'grupo3@trybe.com';
+    await user.type(emailInput, USER_EMAIL);
+    await user.type(passwordInput, '1234567');
+    await user.click(submitBtn);
+
+    const profileBtn = screen.getByTestId('profile-top-btn');
+    await user.click(profileBtn);
+
+    const emailDisplay = screen.getByText(USER_EMAIL);
+    expect(emailDisplay).toBeInTheDocument();
+
+    const doneRecipesBtn = screen.getByTestId('profile-done-btn');
+    const favRecipesBtn = screen.getByTestId('profile-favorite-btn');
+    const logoutBtn = screen.getByTestId('profile-logout-btn');
+
+    expect(doneRecipesBtn).toBeInTheDocument();
+    expect(favRecipesBtn).toBeInTheDocument();
+    expect(logoutBtn).toBeInTheDocument();
+  });
+
+  it('O botão "Done Recipes" redireciona para a rota correta', async () => {
+    renderWithRouterAndRedux(<App />, '/profile');
+
+    const doneRecipesBtn = screen.getByTestId('profile-done-btn');
+    await user.click(doneRecipesBtn);
+
+    expect(window.location.pathname).toBe('/done-recipes');
+  });
+
+  it('O botão "Favorite Recipes" redireciona para a rota correta', async () => {
+    renderWithRouterAndRedux(<App />, '/profile');
+
+    const favRecipesBtn = screen.getByTestId('profile-favorite-btn');
+    await user.click(favRecipesBtn);
+    expect(window.location.pathname).toBe('/favorite-recipes');
+  });
+
+  it('O botão "Logout" limpa o localStorage e volta para a página de login', async () => {
+    renderWithRouterAndRedux(<App />);
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitBtn = screen.getByTestId('login-submit-btn');
+
+    const USER_EMAIL = 'grupo3@trybe.com';
+    await user.type(emailInput, USER_EMAIL);
+    await user.type(passwordInput, '1234567');
+    await user.click(submitBtn);
+
+    const profileBtn = screen.getByTestId('profile-top-btn');
+    await user.click(profileBtn);
+
+    const logoutBtn = screen.getByTestId('profile-logout-btn');
+    await user.click(logoutBtn);
+
+    expect(window.localStorage).toBe(null);
+  });
+});

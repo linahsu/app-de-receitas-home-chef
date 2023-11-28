29593f1c00c9f798c45476a3474f8956a3ca8c3f
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-11-28 19:12:30 -0300

    Adiciona email e botões na página de perfil (req. 56)

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,19 @@
 import Footer from '../components/Footer';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function Profile() {
+  const userEmail = JSON.stringify(useLocalStorage('user')[0].email)
+
   return (
     <div>
-      <h1>Profile</h1>
+      <h3 data-testid="profile-email">{ userEmail.replace(/"/g, '') }</h3>
+
+      <div>
+        <button data-testid="profile-done-btn">Done recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
+
       <Footer />
     </div>
   );

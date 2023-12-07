4ca93f0a5210a0b67b75c276739a9f4dc50d4c3f
Lina Hsu <linahthsu@gmail.com>
2023-12-07 15:34:22 -0300

    Implementa o mockLocalStorage na pasta de mocks

--- src/tests/Req-36-ao-42.test.tsx
+++ src/tests/Req-36-ao-42.test.tsx
@@ -3,6 +3,10 @@ import { vi } from 'vitest';
 import renderWithRouterAndRedux from '../utils/renderWithRouterAndRedux';
 import App from '../App';
 import { fetchDetailMock } from './mocks/fetchMock';
+import mockLocalStorage from './mocks/mockLocalStorage';
+
+const mealInProgressPath = '/meals/52771/in-progress';
+const drinkInProgressPath = '/drinks/718319/in-progress';
 
 describe('Testa a renderização dos elemento e chamadas das APIs da página RecipeInProgress', () => {
   beforeEach(() => {
@@ -10,7 +14,7 @@ describe('Testa a renderização dos elemento e chamadas das APIs da página Rec
   });
 
   it('Testa se a página de meals contém os elementos esperados', async () => {
-    renderWithRouterAndRedux(<App />, '/meals/52771/in-progress');
+    renderWithRouterAndRedux(<App />, mealInProgressPath);
 
     expect(global.fetch).toHaveBeenCalled();
     expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771');
@@ -35,7 +39,7 @@ describe('Testa a renderização dos elemento e chamadas das APIs da página Rec
   });
 
   it('Testa se a página de drinks contém os elementos esperados', async () => {
-    renderWithRouterAndRedux(<App />, '/drinks/718319/in-progress');
+    renderWithRouterAndRedux(<App />, drinkInProgressPath);
 
     expect(global.fetch).toHaveBeenCalled();
     expect(global.fetch).toHaveBeenCalledWith('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=718319');
@@ -65,8 +69,8 @@ describe('Testa o botão de compartilhamento', () => {
     vi.spyOn(global, 'fetch').mockImplementation(fetchDetailMock as any);
   });
 
-  it('Testa o componente MealInProgress', async () => {
-    const { user } = renderWithRouterAndRedux(<App />, '/meals/52771/in-progress');
+  it('Testa se ao clicar no shareBtn, o texto "Link copied!" aparece na tela no componente MealInProgress', async () => {
+    const { user } = renderWithRouterAndRedux(<App />, mealInProgressPath);
 
     const mealShareBtn = await screen.findByTestId(/share-btn/i);
 
@@ -77,8 +81,28 @@ describe('Testa o botão de compartilhamento', () => {
     expect(await navigator.clipboard.readText()).toBe('http://localhost:3000/meals/52771');
   });
 
-  it('Testa o componente DrinkInProgress', async () => {
-    const { user } = renderWithRouterAndRedux(<App />, '/drinks/718319/in-progress');
+  // it('Testa se ao clicar no shareBtn, a url não é copiada no clipboard no componente MealsInProgress', async () => {
+  //   const onCopyFailureMock = vi.fn(() => { window.alert('Failed to copy the link!'); });
+
+  //   const { user } = renderWithRouterAndRedux(<App />, mealInProgressPath);
+
+  //   const mealShareBtn = await screen.findByTestId(/share-btn/i);
+
+  //   expect(mealShareBtn).toBeInTheDocument();
+
+  //   await user.click(mealShareBtn);
+  //   expect(onCopyFailureMock).toHaveBeenCalled();
+  //   expect(await navigator.clipboard.readText()).toBe('');
+  //   expect(screen.getByText(/Link copied!/i)).not.toBeInTheDocument();
+  //   expect(screen.getByText(/Failed to copy the link!/i)).toBeInTheDocument();
+  // });
+
+  // it('Testa se ao clicar no shareBtn, a url não é copiada no clipboard no componente DrinkInProgress', async () => {
+
+  // });
+
+  it('Testa se ao clicar no shareBtn, o texto "Link copied!" aparece na tela no componente DrinkInProgress', async () => {
+    const { user } = renderWithRouterAndRedux(<App />, drinkInProgressPath);
 
     const mealShareBtn = await screen.findByTestId(/share-btn/i);
 
@@ -87,5 +111,32 @@ describe('Testa o botão de compartilhamento', () => {
     await user.click(mealShareBtn);
     expect(screen.getByText(/Link copied!/i)).toBeInTheDocument();
     expect(await navigator.clipboard.readText()).toBe('http://localhost:3000/drinks/718319');
+  });
+});
+
+describe('Testa o botão de favoritar', () => {
+  beforeEach(() => {
+    vi.spyOn(global, 'fetch').mockImplementation(fetchDetailMock as any);
+    mockLocalStorage();
+  });
+
+  it('Testa se ao clicar no favoriteBtn, a receita é adicionada ao favoriteRecipes no LocalStorage no componente MealInProgress', async () => {
+    const { user } = renderWithRouterAndRedux(<App />, mealInProgressPath);
+
+    const mealFavoriteBtn = await screen.findByTestId(/favorite-btn/i);
+
+    expect(mealFavoriteBtn).toBeInTheDocument();
+
+    await user.click(mealFavoriteBtn);
+  });
+
+  it('Testa se ao clicar no favoriteBtn, a receita é adicionada ao favoriteRecipes no LocalStorage no componente DrinkInProgress', async () => {
+    const { user } = renderWithRouterAndRedux(<App />, drinkInProgressPath);
+
+    const mealFavoriteBtn = await screen.findByTestId(/favorite-btn/i);
+
+    expect(mealFavoriteBtn).toBeInTheDocument();
+
+    await user.click(mealFavoriteBtn);
   });
 });
---
+++ src/tests/mocks/mockLocalStorage.tsx
@@ -0,0 +1,73 @@
+const mockFavoriteRecipes = [
+  {
+    id: '52771',
+    type: 'meal',
+    area: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  },
+  {
+    id: '718319',
+    type: 'drink',
+    area: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Aquamarine',
+    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+  },
+];
+
+const mockInProgressRecipes = {
+  cocktails: {
+    718319: ['0', '1'],
+  },
+  meals: {
+    52771: ['2'],
+  },
+};
+
+const mockDoneRecipes = [
+  {
+    id: '52839',
+    type: 'meal',
+    area: 'Italian',
+    category: 'Pasta',
+    alcoholicOrNot: undefined,
+    name: 'Chilli prawn linguine',
+    image: 'https://www.themealdb.com/images/media/meals/usywpp1511189717.jpg',
+    doneDate: '27-03-2023',
+    tags: [],
+  },
+  {
+    id: '52857',
+    type: 'meal',
+    area: 'American',
+    category: 'Dessert',
+    alcoholicOrNot: undefined,
+    name: 'Pumpkin Pie',
+    image: 'https://www.themealdb.com/images/media/meals/usuqtp1511385394.jpg',
+    doneDate: '28-03-2023',
+    tags: ['Halloween', 'Pie', 'Desert'],
+  },
+  {
+    id: '11157',
+    type: 'drink',
+    area: '',
+    category: 'Ordinary Drink',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Brandy Cobbler',
+    image: 'https://www.thecocktaildb.com/images/media/drink/5xgu591582580586.jpg',
+    doneDate: '29-03-2023',
+    tags: [],
+  },
+];
+
+const mockLocalStorage = () => {
+  localStorage.setItem('favoriteRecipes', JSON.stringify(mockFavoriteRecipes));
+  localStorage.setItem('inProgressRecipes', JSON.stringify(mockInProgressRecipes));
+  localStorage.setItem('doneRecipes', JSON.stringify(mockDoneRecipes));
+};
+
+export default mockLocalStorage;

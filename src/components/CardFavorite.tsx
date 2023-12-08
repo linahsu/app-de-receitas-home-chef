38a916bfcda90c69dfd34719f8afd5aaea4a989a
Emilly Stephany Alves Costa <emillystephanyalvescosta@gmail.com>
2023-12-08 20:56:45 -0300

    Req 49 ao 55 - todos os requisitos feitos, falta apenas os testes e resolver os erros de lint

--- src/components/CardFavorite.tsx
+++ src/components/CardFavorite.tsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 import { FavoriteRecipeType } from '../types';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
-function CardFavorite({ recipe }: { recipe: FavoriteRecipeType[] }) {
-  const [favoriteRecipe, setFavoriteRecipe] = useState(recipe);
+function CardFavorite({ recipe,
+  index,
+  handleFavoriteBtn }:
+{ recipe: FavoriteRecipeType,
+  index: number,
+  handleFavoriteBtn: () => void }) {
   const [isCopied, setIsCopied] = useState(false);
 
   const handleShareBtn = (type: string, id: string) => {
@@ -21,51 +25,43 @@ function CardFavorite({ recipe }: { recipe: FavoriteRecipeType[] }) {
 
   return (
     <div>
-      {favoriteRecipe.length > 0 ? (
-        <>
-          {favoriteRecipe.map((favorite, index) => (
-            <div key={ favorite.id }>
-              <Link to={ `/${favorite.type}s/${favorite.id}` }>
-                <img
-                  src={ favorite.image }
-                  alt="Foto da comida"
-                  style={ { width: '300px' } }
-                  data-testid={ `${index}-horizontal-image` }
-                />
-                <h2 data-testid={ `${index}-horizontal-name` }>{favorite.name}</h2>
-              </Link>
-              {favorite.type === 'meal' ? (
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  {`${favorite.nationality} - ${favorite.category}`}
-                </p>
-              ) : (
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  {`${favorite.alcoholicOrNot}`}
-                </p>
-              )}
-              <button>
-                <img
-                  data-testid={ `${index}-horizontal-favorite-btn` }
-                  src={ blackHeartIcon }
-                  alt="favorite Icon"
-                />
-              </button>
-              <button
-                onClick={ () => handleShareBtn(favorite.type, favorite.id) }
-              >
-                <img
-                  data-testid={ `${index}-horizontal-share-btn` }
-                  src="/src/images/shareIcon.svg"
-                  alt="Share icon"
-                />
-              </button>
-              {isCopied && <p>Link copied!</p>}
-            </div>
-          ))}
-        </>
-      ) : (
-        <p>Nenhuma receita encontrada.</p>
-      )}
+      <div key={ recipe.id }>
+        <Link to={ `/${recipe.type}s/${recipe.id}` }>
+          <img
+            src={ recipe.image }
+            alt="Foto da comida"
+            style={ { width: '300px' } }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+        </Link>
+        {recipe.type === 'meal' ? (
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            {`${recipe.nationality} - ${recipe.category}`}
+          </p>
+        ) : (
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            {`${recipe.alcoholicOrNot}`}
+          </p>
+        )}
+        <button onClick={ handleFavoriteBtn }>
+          <img
+            data-testid={ `${index}-horizontal-favorite-btn` }
+            src={ blackHeartIcon }
+            alt="Black heart icon"
+          />
+        </button>
+        <button
+          onClick={ () => handleShareBtn(recipe.type, recipe.id) }
+        >
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src="/src/images/shareIcon.svg"
+            alt="Share icon"
+          />
+        </button>
+        {isCopied && <p>Link copied!</p>}
+      </div>
     </div>
   );
 }
--- src/pages/FavoriteRecipes.tsx
+++ src/pages/FavoriteRecipes.tsx
@@ -4,9 +4,14 @@ import { FavoriteRecipeType } from '../types';
 import CardFavorite from '../components/CardFavorite';
 
 function FavoriteRecipes() {
-  const [favoriteRecipes] = useLocalStorage('favoriteRecipes', []);
+  const [favoriteRecipes, setFavoriteRecipes] = useLocalStorage('favoriteRecipes', []);
   const [showFavorites,
     setShowFavorites] = useState<Array<FavoriteRecipeType>>(favoriteRecipes);
+
+  const checkFavorite = (recepeId: string) => {
+    return favoriteRecipes
+      .some((favorite: FavoriteRecipeType) => favorite.id === recepeId);
+  };
 
   const handleClickFilter = (filter: string) => {
     setShowFavorites(favoriteRecipes);
@@ -17,6 +22,32 @@ function FavoriteRecipes() {
           .filter((favorite: FavoriteRecipeType) => favorite.type === filter),
       );
     }
+  };
+
+  const handleFavoriteBtn = () => {
+    { showFavorites.map((recipes) => {
+      if (recipes.id && recipes.type) {
+        if (!checkFavorite(recipes.id)) {
+          setFavoriteRecipes([
+            ...favoriteRecipes,
+            {
+              id: recipes.id,
+              type: recipes.type,
+              nationality: recipes.nationality || '',
+              category: recipes.category || '',
+              alcoholicOrNot: recipes?.alcoholicOrNot || '',
+              name: recipes.name,
+              image: recipes.image,
+            },
+          ]);
+        } else {
+          const favoriteList = favoriteRecipes
+            .filter((favorite: FavoriteRecipeType) => favorite.id !== recipes.id);
+          setFavoriteRecipes(favoriteList);
+          setShowFavorites(favoriteList);
+        }
+      }
+    }); }
   };
 
   return (
@@ -46,7 +77,16 @@ function FavoriteRecipes() {
         </button>
       </div>
       <div>
-        <CardFavorite recipe={ showFavorites } />
+        {showFavorites.length > 0
+          ? (
+            showFavorites.map((recipe, index) => (<CardFavorite
+              recipe={ recipe }
+              index={ index }
+              handleFavoriteBtn={ handleFavoriteBtn }
+              key={ recipe.id }
+            />))) : (
+              <p>Nenhuma receita encontrada.</p>
+          )}
       </div>
     </>
   );
